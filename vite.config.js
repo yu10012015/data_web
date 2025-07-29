6fa179b8c66b2cf35c05ce8207c05c135d7e82ed
yu10012015 <1752692520@qq.com>
2025-07-29 20:42:51 +0800

    提交配置文件

--- vite.config.js
+++ vite.config.js
@@ -8,18 +8,11 @@ import Components from 'unplugin-vue-components/vite'
 import { viteMockServe  } from 'vite-plugin-mock'
 // https://vite.dev/config/
 export default defineConfig({
+  base:'',
   server: {
     host: 'localhost',
     port: 5173,
     https: false,
-    proxy: {
-      '/api': {
-        target:'http://192.168.82.215/webNerve/modules/itservice/CostsAnalytics/liwenyan/LunGengDataSystem/',
-        changeOrigin:true,
-        rewrite:(path)=>path.replace (/^\/api/,'')
-        
-      },
-    }, 
     open: true
   },
   plugins: [
@@ -50,4 +43,30 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    // target: 'es2020',  //浏览器兼容性 默认 'modules'，这是指 支持原生 ES 模块的浏览器
+    cssCodeSplit: false,  //false 表示整个项目中的所有 CSS 将被提取到一个 CSS 文件中
+    sourcemap: false,  //是否生成sourcemap文件
+    terserOptions: {
+      compress: {
+        drop_console: true, // 生产环境移除console
+        drop_debugger: true // 生产环境移除debugger
+      }
+    },
+    chunkSizeWarningLimit:600, //包报警的最大设置 默认是500
+    rollupOptions: {
+      treeshake: true,   //默认就是true
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+               if(!id.includes("naive-ui")){
+                return "common";
+               }else{ 
+                  return id.toString().split('node_modules/')[1].split('/')[0].toString()      
+               }
+          }
+        }  
+      }
+    }
+  }
 })
