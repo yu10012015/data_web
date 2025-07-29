a0062851df9fed5ddffa9c768781865a494145b4
yu10012015 <1752692520@qq.com>
2025-07-29 15:35:04 +0800

    Update request.js

--- src/api/request.js
+++ src/api/request.js
@@ -1,18 +1,13 @@
 import axios from "axios"
 let request=axios.create();
-
 request.interceptors.request.use(function (config) {
-     config?.params? console.log(config.params):'';
-    
-    
+     config?.params? console.log(config.params):'';  //方便检查参数传递的正确性 生产环境下关闭console
     // 在发送请求之前做些什么
     return config;
   }, function (error) {
     // 对请求错误做些什么
     return Promise.reject(error);
   });
-
-
   request.interceptors.response.use(function (response) {
     // 2xx 范围内的状态码都会触发该函数。
     // 对响应数据做点什么
